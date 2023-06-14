4435df99aa834af83a66eb72f2b75e0cb979c6e3
sarah-die <saridi@alice.de>
2023-06-14 12:31:16 +0200

    edited chart: the component gets a title. Legend styling changed. Add autocolors

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,9 +1,34 @@
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import { CategoryScale } from "chart.js";
-ChartJS.register(CategoryScale);
+import autocolors from "chartjs-plugin-autocolors";
+ChartJS.register(CategoryScale, autocolors);
 
-export default function PieChart({ chartData }) {
-  // options={} is optional
-  return <Pie data={chartData} options={{ maintainAspectRatio: false }} />;
+export default function PieChart(props: { chartData; title: string }) {
+  const options: any = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      autocolors: {
+        mode: "label",
+      },
+      legend: {
+        display: true,
+        align: "center",
+        labels: {
+          pointStyle: "circle",
+          usePointStyle: true,
+          boxHeight: 8,
+          fontColor: "#323130",
+          fontSize: 14,
+        },
+      },
+      title: {
+        display: true,
+        text: props.title,
+      },
+    },
+  };
+
+  return <Pie data={props.chartData} options={options} />;
 }
