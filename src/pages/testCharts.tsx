0a989f95f0b1f615d9e922a538015c1c7c927863
sarah-die <saridi@alice.de>
2023-06-19 10:24:51 +0200

    Refactored useState to useMemo (chartData).

--- src/pages/testCharts.tsx
+++ src/pages/testCharts.tsx
@@ -1,33 +1,38 @@
 import Layout from "../components/layout";
 import Head from "next/head";
 import BarChart from "../components/basicCharts/BarChart";
-import { useState } from "react";
+import { useMemo } from "react";
 import { Userdata, UserData } from "@/data/testData";
 import LineChart from "../components/basicCharts/LineChart";
 import PieChart from "../components/basicCharts/PieChart";
 import ChartWithCustomLegend from "@/components/basicCharts/ChartWithCustomLegend";
 import { ChartData } from "chart.js";
 
+/** Test component that visualizes different types of charts. The dataset is just mock-data. */
+
 export default function TestCharts() {
-  // chart js requires an object that looks like the initialState
-  const [userData, setUserData] = useState<ChartData<"pie">>({
-    labels: UserData.map((data: Userdata) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data: Userdata) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 1,
-      },
-    ],
-  });
+  // chart js requires an object that looks like this
+  const chartData = useMemo<ChartData<"pie">>(
+    () => ({
+      labels: UserData.map((data: Userdata) => data.year),
+      datasets: [
+        {
+          label: "Users Gained",
+          data: UserData.map((data: Userdata) => data.userGain),
+          backgroundColor: [
+            "rgba(75,192,192,1)",
+            "#ecf0f1",
+            "#50AF95",
+            "#f3ba2f",
+            "#2a71d0",
+          ],
+          borderColor: "black",
+          borderWidth: 1,
+        },
+      ],
+    }),
+    [UserData]
+  );
 
   return (
     <Layout>
@@ -49,17 +54,17 @@ export default function TestCharts() {
             calculate the height of charts.
           </div>
           <div className="chart-container">
-            <BarChart chartData={userData} title={"basic bar chart"} />
+            <BarChart chartData={chartData} title={"basic bar chart"} />
           </div>
           <div className="chart-container">
-            <LineChart chartData={userData} />
+            <LineChart chartData={chartData} />
           </div>
           <div className="chart-container">
-            <PieChart chartData={userData} title={"basic bar chart"} />
+            <PieChart chartData={chartData} title={"basic bar chart"} />
           </div>
           <div className="chart-container">
             <ChartWithCustomLegend
-              chartData={userData}
+              chartData={chartData}
               title={"This is the titel"}
             />
           </div>
